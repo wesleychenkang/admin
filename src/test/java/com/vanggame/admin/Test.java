db7f4caa39676be1ee17d23fa6cac6fd6510c197
kangzhi.chen <309592418@qq.com>
2018-07-16 23:03:59 +0800

    test

--- src/test/java/com/vanggame/admin/Test.java
+++ src/test/java/com/vanggame/admin/Test.java
@@ -1,11 +1,14 @@
 package com.vanggame.admin;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.context.request.NativeWebRequest;
 
 import com.vanggame.admin.entity.Admin;
 import com.vanggame.admin.entity.PayWay;
@@ -34,21 +37,42 @@ public class Test {
 	public void test2() {
 		System.out.println("======");
 		List<PayWayChannel> list = pwService.getAllPayWayChannel();
-		
+
 		for (int i = 0; i < list.size(); i++) {
-			List<PayWay> w = list.get(0).getPayWay(); 
+			List<PayWay> w = list.get(0).getPayWay();
 			System.out.println(w.get(0).getPayWayName());
 			System.out.println(list.get(i).tJsonObject());
 		}
-		
-		System.out.println("test2===="+list.size());
+
+		System.out.println("test2====" + list.size());
 	}
-	
-	
+
 	@org.junit.Test
-	public void test3(){
-		
-		System.out.println("count="+ pwService.getAllPayChannelCount());
+	public void test3() {
+
+		PayWayChannel channel = new PayWayChannel();
+		channel.setChannelMasterId(3008);
+		channel.setChannelScid("test");
+		channel.setCreateTime(new Date());
+		channel.setUpdateTime(new Date());
+		channel.setGameId(10008);
+		channel.setPermission("wesley");
+		channel.setVachar(false);
+		channel.setSdkVer("1234");
+		pwService.savePayWayChannel(channel);
+
+		System.out.println("channel" + channel.getTid());
+
+	}
+
+	@org.junit.Test
+	public void test4() {
+		List<String> types = new ArrayList<String>();
+		types.add("1");
+		types.add("2");
+		types.add("3");
+		pwService.savePayVsChannel(types, 3);
+
 	}
 
 }
